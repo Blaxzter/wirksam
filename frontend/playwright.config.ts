a29c61039f26880c027bdeb01805d8111a695535
Frederic Abraham <mail@fabraham.dev>
2026-04-05 19:00:57 +0200

    fix: update Playwright reporter configuration for CI environments

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -36,7 +36,7 @@ export default defineConfig({
   /* Run tests within each file in parallel. */
   fullyParallel: true,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: process.env.CI ? 'blob' : 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
